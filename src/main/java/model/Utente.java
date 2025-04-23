a2271c2d6f14a2d470178de0cb2a355fc1ff01ae
V <v.oratore@studenti.unina.it>
2025-04-23 14:09:52 +0200

    //

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -1,29 +1,44 @@
 package model;
-
-/**
- * The type Utente.
- */
-public class Utente {
-    private final String login;
+import java.util.ArrayList;
+public abstract class Utente {
+    private  String login;
     private String password;
+    private ArrayList<ToDo> ToDo = new ArrayList<>();
 
-    /**
-     * Instantiates a new Utente.
-     *
-     * @param login    the login
-     * @param password the password
-     */
+
     public Utente(String login, String password) {
         this.login = login;
         this.password = password;
     }
 
-    /**
-     * Gets login.
-     *
-     * @return the login
-     */
     public String getLogin() {
         return login;
     }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    //prototipi
+    public abstract boolean login();
+    public abstract void logout();
+    public abstract void aggiungiBacheca();
+    public abstract void modificaBacheca();
+    public abstract void eliminaBacheca();
+    public abstract void modificaOrdine();
+    public abstract void creaTodo();
+    public abstract void modificaTodo();
+    public abstract void eliminaTodo();
+    public abstract void spostaTodo();
+    public abstract void cambiaBacheca();
+    public abstract void condividiTodo();
+    public abstract void revocaCondivisione();
+    public abstract void ricercaTodo();
+    public abstract void todoInScadenza();
+
 }
